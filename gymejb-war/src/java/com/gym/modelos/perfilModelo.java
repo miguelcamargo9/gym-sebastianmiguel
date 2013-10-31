869eb0561a9b17dab9b7f9437800bde8df4d0181
azulsebas123 <azulsebas123@gmail.com>
2013-10-31 06:39:59 +0000

    nuevo crear perfiles

---
+++ gymejb-war/src/java/com/gym/modelos/perfilModelo.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gym.modelos;
+
+/**
+ *
+ * @author Sebastian Rojas
+ */
+public class perfilModelo {
+  
+}
